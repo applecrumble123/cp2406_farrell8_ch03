be48528868af0edfec02f5748e1afb399f06f622
applecrumble123 <30610249+applecrumble123@users.noreply.github.com>
2018-07-23 14:22:36 +0800

    Done

---
+++ DebuggingExercises/FixDebugThree4.java
@@ -0,0 +1,25 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+// This program calculates tutition bills as credits times rate per credit hour
+public class FixDebugThree4
+{
+   public static void main(String args[])
+   {
+      int myCredits = 13;
+      int yourCredits = 17;
+      double rate = 75.84;
+      System.out.println("My tuition:");
+      tuitionBill(myCredits, rate);
+      System.out.println("Your tuition:");
+      tuitionBill(yourCredits, rate);
+   }
+   private static void tuitionBill(int c, double r)
+   {
+      System.out.println("Total due " + String.format("%.2f",(r*c)));
+
+      NumberFormat nf = new DecimalFormat("###.##");
+      System.out.println("Total due " + (nf.format(r*c)));
+
+   }
+}
